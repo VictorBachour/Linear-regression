7e55164aa49f28b2fef3624fab5ded9fe437b5d2
vbacho <victorbachour@gmail.com>
2025-03-10 21:31:46 -0700

    r2 score of .8062059581570026

--- linear.py
+++ linear.py
@@ -1,18 +1,36 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split  # For splitting data
-from sklearn.linear_model import LinearRegression  # For the regression model
 from sklearn.metrics import mean_squared_error, r2_score  # For model evaluation
 from sklearn.datasets import fetch_california_housing
+from sklearn.preprocessing import StandardScaler
 
 data = fetch_california_housing()
 df = pd.DataFrame(data.data, columns=data.feature_names)
 df['Target'] = data.target
-print(df.isnull().sum())
 
 X = df.drop(columns=['Target'])
 y = df['Target']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+model = RandomForestRegressor()
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print(f'Mean Squared Error: {mse}')
+print(f'R² Score: {r2}')
+
+plt.scatter(y_test, y_pred, alpha=.5)
+plt.xlabel("Actual Prices")
+plt.ylabel("Predicted prices")
+plt.title("Actual vs Predicted Prices")
+plt.show()
